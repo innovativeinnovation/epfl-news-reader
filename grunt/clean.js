11575d6b4d97f0985fd120fb5e5c5032bd07866e
William Belle <william.belle@gmail.com>
2017-05-20 22:08:21 +0200

    clean www folder before build

---
+++ grunt/clean.js
@@ -0,0 +1,10 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017.
+ * See the LICENSE file for more details.
+ */
+
+module.exports = {
+  build: {
+    src: ['www'],
+  },
+};
