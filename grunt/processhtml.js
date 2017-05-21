2be6e0f1c97ac23caaa2b8722c4cbd28536099d4
William Belle <william.belle@gmail.com>
2017-05-21 19:59:21 +0200

    add html process

---
+++ grunt/processhtml.js
@@ -0,0 +1,12 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017.
+ * See the LICENSE file for more details.
+ */
+
+module.exports = {
+  release: {
+    files: {
+      'www/index.html': ['src/index.html'],
+    },
+  },
+};
