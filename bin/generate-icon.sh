2631250e2f985779814cce2a119f882c0a027d85
William Belle <william.belle@gmail.com>
2017-05-21 10:51:47 +0200

    add 512x512 icon

--- bin/generate-icon.sh
+++ bin/generate-icon.sh
@@ -14,9 +14,9 @@ which optipng > /dev/null
 if [ 0 -ne $? ]; then echo -e 'optipng not found. Install optipng'; exit 1; fi
 
 # Generate App icon
-target=( icon-256 icon-192 icon-152 icon-144 icon-128 )
-draw=("roundrectangle 0,0,255,255,30,30" "roundrectangle 0,0,191,191,30,30" "roundrectangle 0,0,151,151,20,20" "roundrectangle 0,0,143,143,20,20" "roundrectangle 0,0,128,128,20,20")
-size=(256 192 152 144 128)
+target=( icon-512 icon-256 icon-192 icon-152 icon-144 icon-128 )
+draw=("roundrectangle 0,0,511,511,50,50" "roundrectangle 0,0,255,255,30,30" "roundrectangle 0,0,191,191,30,30" "roundrectangle 0,0,151,151,20,20" "roundrectangle 0,0,143,143,20,20" "roundrectangle 0,0,128,128,20,20")
+size=(512 256 192 152 144 128)
 for ((i=0; i < ${#target[@]}; i++))
 do
   convert -density 72 -resize "${size[$i]}"x"${size[$i]}" assets/gimp/news-icon.png $BUILD_DIR/raw-icon.png
