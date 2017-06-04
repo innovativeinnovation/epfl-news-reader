624828fd51007661244991e30a734c2814265de9
William Belle <william.belle@gmail.com>
2017-06-04 20:40:56 +0200

    clean copyright

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,8 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017.
+ * See the LICENSE file for more details.
+ */
+
 'use strict';
 
 var dataCacheName = 'epfl-news-reader-data';
